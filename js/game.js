c07087e6b75fbc58a2d440441604985e9df8f3fc
Dmytro Labenskyy <d.mon911@gmail.com>
2024-07-23 23:32:22 +0100

    add initial files

---
+++ js/game.js
@@ -0,0 +1,37 @@
+class Game {
+  // code to be added
+  constructor(startScreen, gameScreen, gameEndScreen) {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 1388;
+    this.width = 927;
+    this.enemy = [];
+    this.score = 0;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrequency = 60;
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+    this.update();
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+  update() {
+    console.log("in the update");
+  }
+}
